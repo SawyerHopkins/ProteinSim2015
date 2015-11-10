fc10d7af6c04839ced3af3e5ad3656ecf8c06c86
Sawyer Hopkins <sawyer@sawyerhopkins.com>
2015-11-09 21:19:30 -0600

    Wrote automation for build chain

---
+++ manageBuild.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+function add {
+    echo "$1" >> .automation/forceList
+}
+function remove {
+    sed -i "/$1/d" .automation/forceList
+}
+function build {
+    readarray -t forces < .automation/forceList
+    #Write the core complier from template
+    sed -n -e "1,10w buildAll.sh" .automation/buildAll.template
+    #Write the force compilers from template
+    for var in "${forces[@]}"
+    do
+	echo "Writing ${var}"
+        echo "$(sed -n '/#FORCE/,/#END/p' .automation/buildAll.template)" >> buildAll.sh
+        sed -i -e "s;%NAME%;${var};g" buildAll.sh 
+    done
+    #Write the UI compile from template
+    echo "$(sed -n '/#UI/,/#END/p' .automation/buildAll.template)" >> buildAll.sh
+}
+function display {
+    echo
+    cat < .automation/forceList
+    echo
+}
+
+while getopts "a:r:bc" OPTION
+do
+    case $OPTION in
+        a)
+            add $OPTARG
+            ;;
+        r)
+            remove $OPTARG
+            ;;
+        b)
+            build
+            ;;
+        c)
+            display
+            ;;
+    esac
+done
