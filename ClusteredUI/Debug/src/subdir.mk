0f7bc24d716375b74f23ad8fa795d2b2912acbdc
Sawyer Hopkins <sawyer@sawyerhopkins.com>
2016-03-29 12:12:05 -0500

    Added build structure to git

---
+++ ClusteredUI/Debug/src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/analysis.cpp \
+../src/main.cpp \
+../src/runSim.cpp 
+
+OBJS += \
+./src/analysis.o \
+./src/main.o \
+./src/runSim.o 
+
+CPP_DEPS += \
+./src/analysis.d \
+./src/main.d \
+./src/runSim.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++0x -I"../../ClusteredCore/include" -O0 -g3 -Wall -c -fmessage-length=0 -fopenmp -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
